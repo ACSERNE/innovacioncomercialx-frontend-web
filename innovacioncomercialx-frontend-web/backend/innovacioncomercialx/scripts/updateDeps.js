71adcc27a6a07b51138f9f9dbf95d714fe464328
valdez1245 <valdezwyclef@outlook.es>
2025-11-21 15:46:40 -0300

    chore: cleanup duplicated backend folders

--- innovacioncomercialx-frontend-web/backend/innovacioncomercialx/scripts/updateDeps.js
+++
@@ -1,7 +0,0 @@
-const { execSync } = require('child_process');
-
-['./backend', './innovacioncomercialx-frontend-web', './innovacioncomercialx-frontend-mobile'].forEach(dir => {
-  console.log(`🔹 Actualizando dependencias en ${dir}...`);
-  execSync(`cd ${dir} && npx npm-check-updates -u && npm install --legacy-peer-deps`, { stdio: 'inherit' });
-  console.log(`✅ Dependencias actualizadas en ${dir}`);
-});
